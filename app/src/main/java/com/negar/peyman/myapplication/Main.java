158344e093e8b33ad72c10ac448c487ab8fc283d
negara <ne.abolhasani@gmail.com>
2014-12-09 04:13:18 +0330

    main page added

--- app/src/main/java/com/negar/peyman/myapplication/Main.java
+++ app/src/main/java/com/negar/peyman/myapplication/Main.java
@@ -1,42 +1,76 @@
 package com.negar.peyman.myapplication;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
+import com.negar.peyman.myapplication.R;
 
-public class Main extends Activity {
+public class Main extends FragmentActivity {
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 5;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        String id = savedInstanceState.getString("ID");
-//        System.out.println("%%%%%%%");
-//        System.out.println(id);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public Fragment getItem(int position)
+        {
+            ScreenSlidePageFragment fragment = new ScreenSlidePageFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("salaam", "hi");
+            fragment.setArguments(bundle);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
+
+
 }
--- app/src/main/java/com/negar/peyman/myapplication/ScreenSlidePageFragment.java
+++ app/src/main/java/com/negar/peyman/myapplication/ScreenSlidePageFragment.java
@@ -8,18 +8,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class ScreenSlidePageFragment extends Fragment {
-    String stuID;
 
+    String date;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        date = getArguments() != null ? getArguments().getString("salaam") : "fffff";
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.fragment_screen_slide_page, container, false);
 
-        return rootView;
+        View v = inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
+        View tv = v.findViewById(R.id.date_text);
+        ((TextView)tv).setText("Fragment #" + date);
+
+        return v;
+//        ViewGroup rootView = (ViewGroup) inflater.inflate(
+//                R.layout.fragment_screen_slide_page, container, false);
+//
+//
+//        return rootView;
     }
 }
